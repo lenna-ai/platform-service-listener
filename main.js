89e930fa402a83d548b02790749591e8092436a7
dwijulians <dwjlnss@gmail.com>
2023-12-15 13:42:09 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const axios = require('axios');
+const bodyParser = require('body-parser');
+const {stringify} = require('flatted');
+
+const app = express();
+const port = 3030;
+
+app.use(bodyParser.json({limit: '50mb'}));
+
+app.post('/ms/webhook/received', async (req, res) => {
+  const event = req.body.type;
+  const data = req.body;
+
+  await axios.post("https://staging.lenna.ai/app/public/api/webhook/email/received", data)
+    .then(function (res) {
+    console.log("Success ");
+    })
+    .catch(function (error) {
+    console.log(error);
+  });
+
+  res.status(200).json({
+    message: 'success',
+  });
+});
+
+app.post('/ms/webhook/sent', async (req, res) => {
+  const event = req.body.type;
+  const data = req.body;
+
+  console.log("sent ", data);
+  await axios.post("http://platform.lenna.test/app/public/api/webhook/email/set-message-id", data)
+    .then(function (res) {
+      console.log("Message ID updated.");
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+});
+
+app.listen(port, () => {
+  console.log('payload');
+});
