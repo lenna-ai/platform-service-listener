12ea3adb6b458b2644185e23160bf574976ae73b
dwijulians <dwjlnss@gmail.com>
2024-08-06 17:11:25 +0700

    fix

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const app = express();
 const port = 8080;
 app.use(bodyParser.json({limit: '50mb'}));
 
-amqp.connect('amqps://txnploij:HX2L2iwb1K8SO4WG2XbU9X4VthrBTXeM@armadillo.rmq.cloudamqp.com/txnploij')
+amqp.connect('amqps://gctnabkn:O5fogenqWYlIFt6E_8TNDd0WH914UHEL@armadillo.rmq.cloudamqp.com/gctnabkn')
   .then(async conn=> {
     return conn.createChannel().then(ch => {
       // Deklarasi antrian
@@ -17,14 +17,20 @@ amqp.connect('amqps://txnploij:HX2L2iwb1K8SO4WG2XbU9X4VthrBTXeM@armadillo.rmq.cl
       ch.prefetch(1);
       console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", que);
       ch.consume(que, function(msg) {
+        console.log("Message content", JSON.parse(msg.content));
         var secs = msg.content.toString().split('.').length - 1;
+
         setTimeout(function() {
-          console.log(" [x] Done", msg);
+          axios.post('https://staging.lenna.ai/app/public/api/webhook/email/broadcast', JSON.parse(msg.content))
+            .then(function (response) {
+              console.log("response ", response.data);
+            })
+            .catch(function (error) {
+              console.log(error);
+            });
+          console.log(" [x] Done");
         }, secs * 2000);
+        ch.ack(msg);
       });
     })
-}).catch(console.warn);
-
-app.listen(port, () => {
-  console.log('payload');
-});
+}).catch(console.log);
